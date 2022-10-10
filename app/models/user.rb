fb03f7c5d8d1b2cb2b1344615ceb3d9f049fc14f
Akunuri Roshini <roshini.akunuri@acsicorp.com>
2022-10-10 11:38:26 +0530

    changes Roshini

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_and_belongs_to_many :products
-  enum role: [:customer,:manufacture]
+  enum role: [:customer,:manufacture,:agent]
   after_initialize :set_default_role,:if => :new_record?
   def set_default_role
     self.role ||=:user
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_03_091111) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_03_073102) do
   create_table "customers", force: :cascade do |t|
     t.string "email"
     t.string "password"
@@ -24,14 +24,16 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_03_091111) do
     t.integer "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer "warehouse_id"
-    t.integer "pcount_war", default: 0
-    t.integer "pcount_cus", default: 0
   end
 
   create_table "products_users", id: false, force: :cascade do |t|
     t.integer "user_id", null: false
     t.integer "product_id", null: false
+  end
+
+  create_table "products_warehouses", id: false, force: :cascade do |t|
+    t.integer "product_id", null: false
+    t.integer "warehouse_id", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -43,12 +45,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_03_091111) do
     t.integer "role", default: 0
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  end
-
-  create_table "warehouses", force: :cascade do |t|
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
